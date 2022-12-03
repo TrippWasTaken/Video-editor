869e2cfcbceadc6a7b939cf83f262fa0fa0f4959
Konrad Knecht <C16395636@mytudublin.ie>
2022-12-03 11:53:15 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Container from './screens/main/MainContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <Container />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screens/main/MainContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainContainer = () => {
+  return (
+    <div>Container</div>
+  )
+}
+
+export default MainContainer
